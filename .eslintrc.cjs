b81d5f48b958bf93ea521cefb3164ee3596c44de
glf30 <greyson.frazier@gmail.com>
2023-09-21 16:29:22 -0400

    Config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,14 +16,8 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
 
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/consistent-type-imports": "off",
+    "@typescript-eslint/no-unused-vars": "off"
   },
 };
 
